756a4151c86af72ecdfa32f783ddb1c5ae5cebd0
jbijlsma <jeroen_work@yahoo.com>
2022-02-07 11:44:34 +0100

    Add deployment script for AKS

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+docker build -t dnw2022/aspnetapp:latest -f ./Dockerfile .
+echo "$DOCKER_PASSWORD" | docker login -u $DOCKER_ID --password-stdin
+docker push dnw2022/dnw-react:latest
+
+kubectl apply -f k8s
+
+kubectl rollout restart deployment/dnw-react-deployment
